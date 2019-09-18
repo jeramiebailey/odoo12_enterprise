4da02a1b3d302e967d289a95b6a6138091ac376f
Jeramie Bailey <jeramie.bailey09@gmail.com>
2019-09-18 21:09:32 +0300

    add account move show maturity

---
+++ account_move_show_maturity/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2019 Eska Yazılım ve Danışmanlık A.Ş (www.eskayazilim.com.tr)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    'name': 'Show Move Maturity',
+    'summary': 'Always shows move maturity',
+    'version': '12.0.1.0.0',
+    'category': 'Accounting',
+    'author': 'Eska',
+    'website': 'http://www.eskayazilim.com.tr',
+    'license': 'AGPL-3',
+    'depends': [
+        'account',
+    ],
+    'data': [
+        'views/account_move_view.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+}
