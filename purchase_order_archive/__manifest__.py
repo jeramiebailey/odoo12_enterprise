97be258d30169bda34c7ef19ec41088c41b89bc1
Jeramie Bailey <jeramie.bailey09@gmail.com>
2019-09-20 19:51:53 +0300

    updated archive modules to oca

--- purchase_order_archive/__manifest__.py
+++ purchase_order_archive/__manifest__.py
@@ -1,12 +1,11 @@
-# -*- coding: utf-8 -*-
-# Copyright 2017 Onestein (<http://www.onestein.eu>)
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+# Copyright 2017-2018 Onestein (<https://www.onestein.eu>)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 {
     'name': 'Purchase Order Archive',
     'summary': 'Archive Purchase Orders',
-    'author': 'Onestein',
-    'website': 'http://www.onestein.eu',
+    'author': 'Onestein, Odoo Community Association (OCA)',
+    'website': 'https://github.com/OCA/purchase-workflow',
     'category': 'Purchases',
     'version': '12.0.1.0.0',
     'license': 'AGPL-3',
--- sale_order_archive/__manifest__.py
+++ sale_order_archive/__manifest__.py
@@ -1,12 +1,11 @@
-# -*- coding: utf-8 -*-
-# Copyright 2017 Onestein (<http://www.onestein.eu>)
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+# Copyright 2017-2018 Onestein (<https://www.onestein.eu>)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 {
     'name': 'Sale Order Archive',
     'summary': 'Archive Sale Orders',
-    'author': 'Onestein',
-    'website': 'http://www.onestein.eu',
+    'author': 'Onestein, Odoo Community Association (OCA)',
+    'website': 'https://github.com/OCA/sale-workflow',
     'category': 'Sales',
     'version': '12.0.1.0.0',
     'license': 'AGPL-3',
