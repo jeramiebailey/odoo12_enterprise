335ad9c15b16f48e638644aa36ba32fa07b5eab1
Jeramie Bailey <jeramie.bailey09@gmail.com>
2019-09-25 19:20:02 +0300

    sale order total quantities

---
+++ sale_order_total_quantity/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2019 Eska Yazılım ve Danışmanlık A.Ş (www.eskayazilim.com.tr)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    'name': 'Sale Order Total Quantity',
+    'summary': 'Adds Total Quantity on Sale Orders',
+    'version': '12.0.1.0.0',
+    'category': 'Sale',
+    'author': 'Eska',
+    'website': 'http://www.eskayazilim.com.tr',
+    'license': 'AGPL-3',
+    'depends': [
+        'sale',
+    ],
+    'data': [
+        'views/sale_order_view.xml',
+    ],
+    'installable': True,
+}
