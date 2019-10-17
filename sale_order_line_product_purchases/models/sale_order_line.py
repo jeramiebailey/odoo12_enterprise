cb1d21f254eb43955a74138526fd8bd0de8145cb
Jeramie Bailey <jeramie.bailey09@gmail.com>
2019-10-17 17:35:11 +0300

    sorting module and purchase sale modules

---
+++ sale_order_line_product_purchases/models/sale_order_line.py
@@ -0,0 +1,30 @@
+# Copyright 2019 Eska Yazılım ve Danışmanlık A.Ş (www.eskayazilim.com.tr)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from odoo import _, api, models
+
+
+class SaleOrderLine(models.Model):
+    _inherit = 'sale.order.line'
+
+    @api.multi
+    def action_sale_product_purchases(self):
+        id = self.env.ref('sale_order_line_product_purchases.'
+                           'product_purchases_view')
+        lines = self.env['purchase.order.line'].search(
+            [('product_id', '=', self.product_id.id)],
+            order='order_id DESC',
+            limit=10,
+        )
+        return {
+            'name': _('Purchases'),
+            'view_type': 'form',
+            'view_mode': 'tree',
+            'res_model': 'purchase.order.line',
+            'views': [(id.id, 'tree')],
+            'view_id': False,
+            'type': 'ir.actions.act_window',
+            'target': 'new',
+            'domain': "[('id','in',["+','.join(
+                map(str, lines.ids))+"])]",
+        }
