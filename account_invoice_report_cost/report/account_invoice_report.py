15cc59cd4f2678e8cfc1df71d2c9ba7b17f1e2bf
Jeramie Bailey <jeramie.bailey09@gmail.com>
2019-12-18 04:10:00 +0300

    invoice report product cost & partner tags. sales report bring count.

---
+++ account_invoice_report_cost/report/account_invoice_report.py
@@ -0,0 +1,36 @@
+# Copyright 2019 Eska Yazılım ve Danışmanlık A.Ş (www.eskayazilim.com.tr)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from odoo import models, fields
+
+
+class AccountInvoiceReport(models.Model):
+    _inherit = "account.invoice.report"
+
+    price_cost = fields.Float(
+        string='Cost',
+        readonly=True,
+    )
+
+    def _select(self):
+        res = super()._select()
+        res += """
+            ,sub.price_cost as price_cost
+            """
+        return res
+
+    def _sub_select(self):
+        res = super()._sub_select()
+        res += """
+            , SUM(ail.quantity * cost.value_float) AS price_cost
+            """
+        return res
+
+    def _from(self):
+        res = super(AccountInvoiceReport, self)._from()
+        res += """
+                LEFT JOIN ir_property cost ON (cost.name='standard_price' AND 
+                cost.res_id=CONCAT('product.product,',pr.id) AND 
+                cost.company_id=ai.company_id)
+              """
+        return res
