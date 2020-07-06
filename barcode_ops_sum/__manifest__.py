70a1b5de71fcd041ac7c8aef947fa9e4f2d6e5e9
Jeramie Bailey <jeramie.bailey09@gmail.com>
2020-07-06 15:01:16 +0200

    fixes for pos & barcodes

---
+++ barcode_ops_sum/__manifest__.py
@@ -0,0 +1,13 @@
+
+{
+    'name': 'Barcode Ops',
+    'version': '12.0.1.0.0',
+    'category': 'Stock',
+    'license': 'AGPL-3',
+    'depends': [
+        'stock','barcodes'
+    ],
+    'data': [
+        'views/assets.xml',
+    ],
+}
