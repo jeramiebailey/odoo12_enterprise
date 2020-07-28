b8d580db2a0350f6b785a72ea126c584788c2245
Jeramie Bailey <jeramie.bailey09@gmail.com>
2020-07-28 14:38:59 +0200

    [Add]

--- invoice_terms_and_conditions/models/account_invoice.py
+++ invoice_terms_and_conditions/models/account_invoice.py
@@ -1,21 +1,17 @@
 from odoo import models, fields, api
 
 
-class AccountInvoice(models.Model):
+class AccountInvoiceInhr(models.Model):
     _inherit = 'account.invoice'
 
-# updated by marwa osama to update note field (terms and conditions ) automaticaly when invoice created from any place pos/so related to task ECUSA-13
-    @api.model
-    def create(self, vals):
-        res = super(AccountInvoice, self).create(vals)
-        company = self.company_id or self.env.user.company_id
-        v_comment = company.with_context(lang=self.partner_id.lang).sale_note or (
-                    self._origin.company_id == company and self.comment)
-        comment_chk = self.env['account.invoice'].sudo().search([])
-        if comment_chk:
-            for line in comment_chk:
-                if len(line.comment)<=2:
-                    line.write({'comment': v_comment})
-        return res
+    # updated by marwa osama to update note field (terms and conditions ) automaticaly when invoice created from any place pos/so related to task ECUSA-13
+    comment = fields.Text('Additional Information', compute='defualt_comment',readonly=True, states={'draft': [('readonly', False)]})
 
-AccountInvoice()
+    @api.multi
+    def defualt_comment(self):
+        for item in self:
+            item.comment = self.env.user.company_id.sale_note
+        return True
+
+
+AccountInvoiceInhr()
