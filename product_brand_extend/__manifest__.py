7cc84b934cfc4e15087caa07befee39c5a7461b8
Jeramie Bailey <jeramie.bailey09@gmail.com>
2020-09-22 13:21:37 +0200

    [FIX]panorama_dynamic_product_label

--- product_brand_extend/__manifest__.py
+++ product_brand_extend/__manifest__.py
@@ -1,17 +1,15 @@
-
 {
     'name': 'Product Brand Extend',
     'version': '12.0.2.0.0',
     'category': 'Product',
     'license': 'AGPL-3',
     'depends': [
-        'sale',
-        ],
+        'sale', 'product',
+        'emipro_theme_base',
+    ],
     'data': [
         'views/product_template.xml',
     ],
     'installable': True,
     'auto_install': False
 }
-
-
