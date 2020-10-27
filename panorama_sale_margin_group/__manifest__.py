89db162b6d9b266a722f4e89291ec5aa859b1e50
Jeramie Bailey <jeramie.bailey09@gmail.com>
2020-10-27 14:52:06 +0200

    [UPD]pos_workflow_mac5,[NEW]panorama_sale_margin_group

---
+++ panorama_sale_margin_group/__manifest__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "panorama_sale_margin_group",
+    'author': "Centione",
+    'website': "http://www.centione.com",
+    'category': 'Sale Margin',
+    'version': '0.1',
+
+    'depends': ['base', 'sale', 'sale_margin'],
+
+    'data': [
+        # 'security/ir.model.access.csv',
+        'views/views.xml',
+    ]
+}
