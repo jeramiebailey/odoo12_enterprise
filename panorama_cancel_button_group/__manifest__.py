f5e5e11fdc36eedb105d76d46cc8e29db6230d42
Jeramie Bailey <jeramie.bailey09@gmail.com>
2021-01-03 19:42:02 +0200

    [add]panorama_cancel_button_group

---
+++ panorama_cancel_button_group/__manifest__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "panorama_cancel_button_group",
+    'author': "Centione",
+    'website': "http://www.centione.com",
+    'category': 'inventory',
+    'version': '0.1',
+
+    'depends': ['base', 'sale','stock'],
+
+    'data': [
+        # 'security/ir.model.access.csv',
+        'views/views.xml',
+    ]
+}
