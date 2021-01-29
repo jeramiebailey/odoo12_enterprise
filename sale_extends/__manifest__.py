020b6e928f34ec5fbf91bb1258b5d80923555fe6
Jeramie Bailey <jeramie.bailey09@gmail.com>
2021-01-29 22:09:42 +0530

    [IMP]Improved sale_extends module.

--- sale_extends/__manifest__.py
+++ sale_extends/__manifest__.py
@@ -1,15 +1,14 @@
 # -*- coding: utf-8 -*-
 {
-    'name': 'Sale Extends',
+    'name': 'Panorama Sale Extends',
     'category': 'sale',
     'sequence': 1,
-    'summary': 'Sale Extends',
-    'author': 'Omega System',
+    'summary': 'Panorama Sale Extends',
+    'author': '',
     'version': '1.0',
     'description': "",
     'depends': ['crm', 'sale'],
     'data': [
-        # 'views/sale_view.xml',
     ],
     'installable': True,
     'application': True,
--- sale_extends/models/sale.py
+++ sale_extends/models/sale.py
@@ -2,6 +2,15 @@
 from odoo import models, _
 
 
+class SaleOrder(models.Model):
+    _inherit = 'sale.order'
+
+    def update_order_line(self):
+        order_lines = self.env['sale.order.line'].search([])
+        for order_line in order_lines:
+            order_line.name = order_line.product_id.get_product_multiline_description_sale()
+
+
 class ProductProduct(models.Model):
     _inherit = 'product.product'
 
