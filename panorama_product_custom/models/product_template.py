78d20561ad6119987d30c770e2e97f19de17a5fe
Jeramie Bailey <jeramie.bailey09@gmail.com>
2021-01-29 22:10:34 +0530

    [IMP]commented code.

--- panorama_product_custom/models/product_template.py
+++ panorama_product_custom/models/product_template.py
@@ -16,13 +16,13 @@ class ProductTemplate(models.Model):
 class ProductProduct(models.Model):
     _inherit = 'product.product'
 
-    def get_product_multiline_description_sale(self):
-        """ Compute a multiline description of this product, in the context of sales
-                (do not use for purchases or other display reasons that don't intend to use "description_sale").
-            It will often be used as the default description of a sale order line referencing this product.
-        """
-        name = self.display_name
-        if self.commerce_desc:
-            name += '\n' + self.commerce_desc
-
-        return name
+    # def get_product_multiline_description_sale(self):
+    #     """ Compute a multiline description of this product, in the context of sales
+    #             (do not use for purchases or other display reasons that don't intend to use "description_sale").
+    #         It will often be used as the default description of a sale order line referencing this product.
+    #     """
+    #     name = self.display_name
+    #     if self.commerce_desc:
+    #         name += '\n' + self.commerce_desc
+    #
+    #     return name
