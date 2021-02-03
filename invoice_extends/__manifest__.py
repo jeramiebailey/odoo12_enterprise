4e35595a3eea8c154866de3a44f6bd848f0a027c
Jeramie Bailey <jeramie.bailey09@gmail.com>
2021-02-03 21:03:33 +0530

    [ADD]Added module, Track unexpected transactions at expenses.

---
+++ invoice_extends/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Panorama Invoice Extends',
+    'category': 'account',
+    'sequence': 1,
+    'summary': 'Panorama Invoice Extends',
+    'author': '',
+    'version': '1.0',
+    'description': "",
+    'depends': ['account'],
+    'data': [
+        'views/inherit_invoice_view.xml'
+    ],
+    'installable': True,
+    'application': True,
+}
