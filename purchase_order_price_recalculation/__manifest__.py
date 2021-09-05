3d692a28bda85690af9d8a011b33df0960bd27f6
Jeramie Bailey <jeramie.bailey09@gmail.com>
2021-09-05 08:05:53 +0300

    purchase_order_price_recalculation

---
+++ purchase_order_price_recalculation/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2021 Eska Yazılım ve Danışmanlık A.Ş (www.eskayazilim.com.tr)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    'name': 'Purchase Order Price Recalculation',
+    'summary': 'Purchase Order Price Recalculation',
+    'version': '12.0.1.0.0',
+    'category': 'Purchase',
+    'author': 'Eska',
+    'website': 'http://www.eskayazilim.com.tr',
+    'license': 'AGPL-3',
+    'depends': [
+        'purchase',
+    ],
+    'data': [
+        'views/purchase_order_view.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+}
