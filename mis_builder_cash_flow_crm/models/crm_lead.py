ceb2828b827ea25ca392acf95f24e7c0d0e07efd
Jeramie Bailey <jeramie.bailey09@gmail.com>
2021-11-04 12:47:14 +0300

    cash flow delete leads if exist

--- mis_builder_cash_flow_crm/models/crm_lead.py
+++ mis_builder_cash_flow_crm/models/crm_lead.py
@@ -9,6 +9,10 @@ class Lead(models.Model):
     _inherit = 'crm.lead'
 
     def generate_mis_report(self):
+        leads = self.env['mis.cash_flow.forecast_line'].search([
+                ('lead_id', 'in', self.ids)
+        ])
+	leads.unlink()
         for lead in self:
             if lead.date_deadline and lead.partner_id and lead.planned_revenue:
                 self.env['mis.cash_flow.forecast_line'].create({
